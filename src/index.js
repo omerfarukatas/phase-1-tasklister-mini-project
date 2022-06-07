92fd3e61099685322fc1f97a6b4fdd3d46f1526a
Omer <omerfarukatas96@gmail.com>
2022-06-07 22:15:04 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
 });
+
+const input = document.querySelector("#new-task-description");
+input.addEventListener("change", () => {
+  const inputValue = input.value;
+  return inputValue;
+})
+
+const button = document.querySelector("#submitBtn");
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  const newTask = document.createElement("li");
+  newTask.innerHTML = input.value;
+  document.querySelector("#tasks").appendChild(newTask);
+  input.value = "";
+})
